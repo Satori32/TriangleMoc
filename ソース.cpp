52239437c13eef6c700c9b2f5695147070c7dd27
Satori32 <Huga8989+githubf@gmail.com>
2024-05-31 11:03:52 +0900

    add rotate.cheep code.

--- ソース.cpp
+++ ソース.cpp
@@ -7,6 +7,7 @@
 #include <thread>
 
 #include "EscapeSequence.h"
+#include <conio.h>
 
 //i cant write only triangle... fuck.
 
@@ -37,7 +38,7 @@ bool WriteLine(double X1, double X2,double Y, char Color) {
 /**/
 bool WriteTriangle(Triangle X, char Color) {
 	//std::sort(X.begin(), X.end(), [](auto& A, auto& B) {return A.X > B.X; });
-	std::sort(X.begin(), X.end(), [](auto& A, auto& B) {return A.Y < B.Y; });
+	//std::sort(X.begin(), X.end(), [](auto& A, auto& B) {return A.Y < B.Y; });
 
 	Locate(X[0].X, X[0].Y); std::cout << Color;
 	Locate(X[1].X, X[1].Y); std::cout << (char)(Color+1);
@@ -55,17 +56,17 @@ bool WriteTriangle(Triangle X, char Color) {
 	std::intmax_t i=0;
 	double YM = L2;
 	for (i = 1; i <= L2; i++) {
-		WriteLine(Center - (B * i), Center + (C * i), i+X[0].Y, 'A');
+	///	WriteLine(Center - (B * i), Center + (C * i), i+X[0].Y, 'A');
 
 	} 
 	double Z = (DeltaAC - DeltaAB);
 
 	for (double j = 0; j < Z; j += 1) {
 		if (X[1].Y < X[2].Y) {
-			WriteLine(X[1].X + (X[2].X - X[1].X) / Z * j, Center + (C * (i + j)), j + i + X[0].Y, 'B');
+	///		WriteLine(X[1].X + (X[2].X - X[1].X) / Z * j, Center + (C * (i + j)), j + i + X[0].Y, 'B');
 		}
 		else {
-			WriteLine(Center + (C * (j + i)), (X[2].X - X[1].X) / Z * j, j + i + X[0].Y, 'C');
+	//		WriteLine(Center + (C * (j + i)), (X[2].X - X[1].X) / Z * j, j + i + X[0].Y, 'C');
 		}
 	}
 
@@ -104,12 +105,70 @@ bool WriteTriangle(Triangle X, char Color) {
 }
 /**/
 
+bool RotOne(char C) {
+	std::intmax_t X = 16;
+	std::intmax_t Y = 12;
+	double R=8;
+
+	double PI = 3.14926434;
+	double Rad = PI / 180.0;
+
+	double OX = 16;
+	double OY = 16;
+
+	int k = 0;
+
+	for (std::size_t i = 0; i < 360.0*16; i++) {
+		double PX = R * std::sin(Rad * i) + OX;
+		double PY = R * std::cos(Rad * i) + OY;
+		Locate(PX, PY);
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		ClearShow();
+		std::cout << PX<<':'<<PY;
+		if (_kbhit()) {
+			k=_getch();
+		}
+		if (k == 27) { break; }
+	}		
+	return true;
+}
+
+bool RotateTriangle(char C) {
+
+	Triangle T = { Point2D{0,1},Point2D{1,-1},Point2D{ -1,-1 } };
+	double X = 5;
+	double OX = 40;
+	double OY = 10;
+	Triangle T2 = T;
+	double PI = 3.14926434;
+	double Rad = PI / 180.0;
+
+	for (auto& o : T2) {
+		o.X *= X;
+		o.Y *= X;
+	}
+
+	for (std::intmax_t i = 0; i < 360; i++) {
+		auto A = T2;
+		for (auto& o : A) {
+			o.X = std::sin(Rad * i)*o.X;
+			o.Y = std::cos(Rad * i)*o.Y;
+			o.X += OX;//originX
+			o.Y += OY;//originY
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(128));
+		ClearShow();
+		WriteTriangle(A, C);
+
+	}
+	return 0;
+}
+
 bool WriteTriangle(Point2D A, Point2D B, Point2D C, char Color) {
 	return WriteTriangle({A,B,C},Color);
 }
-int main() {
-	SetDisplayMode();
-	//HideCursor();
+
+bool Test1() {
 	ClearShow();
 	Triangle T{Point2D{ 16,1 },{ 1,12 },{ 32,16 }};
 	
@@ -120,9 +179,14 @@ int main() {
 	double Q = 0xdeadbeef;
 
 	WriteTriangle(T, 'A');
+
+	return true;
+}
+
+bool Test2() {
 	/**/
 	std::mt19937 mt(1); 
-	std::uniform_int_distribution<intmax_t> ui(0, 21);
+	std::uniform_int_distribution<std::uint32_t> ui(0, 21);
 
 	for (int i = 0; i < 16; i++) {
 		std::this_thread::sleep_for(std::chrono::seconds(2));
@@ -132,6 +196,16 @@ int main() {
 
 	}
 	/**/
+	return true;
+}
+
+int main() {
+	SetDisplayMode();
+	//HideCursor();
+
+	//RotateTriangle('X');
+
+	//RotOne('X');
 	return 0;
 
 
